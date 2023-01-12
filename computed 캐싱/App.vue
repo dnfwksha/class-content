9e939671ccbf507b911f69fd1022fa30971a000b
Juhwan <dnfwksha@gmail.com>
2023-01-12 17:03:16 +0900

    수업내용

---
+++ computed 캐싱/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{ reverseMessage()}}</h1>
+  <h1>{{ reverseMessage()}}</h1>
+  <h1>{{ reverseMessage()}}</h1>
+  <h1>{{ reverseMessage()}}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello Computed!'
+    }
+  },
+  computed: {
+    reversedMessage() {
+      return this.msg.split('').reverse().join('')
+    }
+  },
+  methods: {
+    reverseMessage() {
+      return this.msg.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.aa {
+  font-size: 100px;
+}
+
+</style>
