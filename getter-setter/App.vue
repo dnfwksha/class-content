0bc9318ca85cf6cb80e882570e85e0356ef70c8b
Juhwan <dnfwksha@gmail.com>
2023-01-12 21:14:19 +0900

    추가

---
+++ getter-setter/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <button @click="add">ADD</button>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello Computed!'
+    }
+  },
+  computed: {
+    //Getter -> 별도로 내용 추가 불가(캐싱)
+    //   reversedMessage() {
+    //     return this.msg.split('').reverse().join('')
+    //   }
+
+    //Getter,Setter
+    reversedMessage: {
+      get() {
+        return this.msg.split('').reverse().join('')
+      },
+      set(value) {
+        this.msg = value
+      }
+    }
+  },
+  methods: {
+    add() {
+      // this.reversedMessage+='!?'
+      this.reversedMessage += '!?'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.aa {
+  font-size: 100px;
+}
+
+</style>
---
+++ 클래스와 스타일 바인딩/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1 :class="{active:isActive}" @click="activate">
+    Hello?!({{isActive}})
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    activate() {
+      this.isActive = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.active {
+  font-size: 100px;
+  color: red;
+  font-weight: bold;
+}
+
+</style>
